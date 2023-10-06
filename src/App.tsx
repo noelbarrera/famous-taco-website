e3da9608c0a2265897dc9f5634e039f51b202c94
Noel Barrera <70553615+noelbarrera@users.noreply.github.com>
2023-10-06 10:17:55 -0400

    wip: added header and start of homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
+import famousTaco from './assets/insidefamoustaco.jpeg'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar/>
+      <section>
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+          <img src={famousTaco} alt="" />
+        </div>
+        <section>
+          second section
+        </section>
+        <section>
+          <div style={{display: 'flex', justifyContent: 'center'}}>
+            <div style={{border: '1px solid black', textAlign: 'center'}}>
+
+            <h3>Contact & Hours</h3>
+            <a href="https://maps.app.goo.gl/4vQnUzz18VfZrVvd6">4104 N Clinton St, Fort Wayne, IN 46805</a>
+            <p>Phone Number: (260)480-2400</p>
+            <p>Monday-Thursday: 10:30am-10:00pm</p>
+            <p>Friday-Saturday: 10:30am-11:00pm</p>
+            <p>Sunday: 10:30pm-11:00pm</p>
+            <div>
+            </div>
+            </div>
+            {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3005.908632190769!2d-85.13336182356257!3d41.11468647133568!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8815e36bbc902f45%3A0xeb60047be5dca273!2sThe%20Famous%20Taco!5e0!3m2!1sen!2sus!4v1696530685627!5m2!1sen!2sus" width="800" height="300" style={{border: '0'}} loading="lazy"></iframe> */}
+            <div>
+            </div>
+          </div>
+        </section>
+      </section>
     </>
   )
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import "../styles/Navbar.css"
+
+const Navbar = () => {
+  return (
+    <nav className='nav'>
+      <div className='nav-title'>
+        Famous Taco
+      </div>
+    <div className='nav-links'> 
+      <ul>
+        <li>
+          <a href="">
+          About
+          </a>
+        </li>
+        <li>
+          <a href="">
+          Contact
+          </a>
+        </li>
+        <li>
+          <a href="">
+          Menu
+          </a>
+        </li>
+        <li>
+          <a href="">
+          Join Our Team
+          </a>
+        </li>
+        <li>
+          <a href="">
+          Locations
+          </a>
+        </li>
+      </ul>
+    </div>
+    </nav>
+  )
+}
+
+export default Navbar
