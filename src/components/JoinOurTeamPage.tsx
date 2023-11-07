4bb6285b2cb22f75ee9064ae85ccfa0c177942eb
Noel Barrera <70553615+noelbarrera@users.noreply.github.com>
2023-11-07 14:41:19 -0500

    wip: setting up form and styles in page

--- src/components/JoinOurTeamPage.tsx
+++ src/components/JoinOurTeamPage.tsx
@@ -1,8 +1,20 @@
-
+import "../styles/JoinOurTeamPage.css";
+//TODO: Add back-end logic to store informations
 const JoinOurTeamPage = () => {
-  return (
-    <div>JoinOurTeamPage</div>
-  )
-}
+  return <div className="page-container">
+    <h1>
+      Work For Us!
+    </h1>
+    <p>Thank you for your interest. Please Fill out the form and upload your resume for consideration.</p>
 
-export default JoinOurTeamPage
+    <form className="page-container-form" >
+    
+      <input type="text" placeholder="Name" required/>
+      <input type="text" placeholder="Phone" required/>
+      <input type="text" placeholder="Email" required/>
+      <button type="submit">Apply</button>
+    </form>
+  </div>;
+};
+
+export default JoinOurTeamPage;
