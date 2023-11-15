75d0a82d45a832c40da4f000999ba48c9b908904
Noel Barrera <70553615+noelbarrera@users.noreply.github.com>
2023-11-15 12:11:21 -0500

    updated dinner menu

--- src/components/TabPanels/DinnerMenu.tsx
+++ src/components/TabPanels/DinnerMenu.tsx
@@ -40,6 +40,31 @@ const DinnerMenu = () => {
           <div className="menu-item-description">Includes our fresh guacamole and choice of beans.</div>
         </div>  
           {/* //TODO: possible add burrito bowl */}
+      <div className="menu-section-title">Taco Platters</div>
+      <div className="menu-item">
+        <span className="menu-item-price">$8.99</span>
+        <div className="menu-item-title">Two Taco Platters</div>
+        <div className="menu-item-description">served with rice and beans</div>
+      </div>
+      <div className="menu-item">
+        <span className="menu-item-price">$10.99</span>
+        <div className="menu-item-title">Three Taco Platters</div>
+        <div className="menu-item-description">served with rice and beans</div>
+      </div>
+      <div className="menu-item">
+        <span className="menu-item-price">$12.49</span>
+        <div className="menu-item-title">Taco Supremos Platters</div>
+        <div className="menu-item-description">Three tacos supremos served with
+rice and beans</div>
+      </div>
+      <div className="menu-item">
+        <span className="menu-item-price">$13.99</span>
+        <div className="menu-item-title">Steak Supremos Platters</div>
+        <div className="menu-item-description">Three steak tacos supremos served with
+rice and beans</div>
+      </div> 
+      
+
     </div>
   );
 };
